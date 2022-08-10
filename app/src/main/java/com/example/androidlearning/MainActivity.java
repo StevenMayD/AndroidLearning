0a3f9fab5f5ce611ac13eaabef052f614c653dd5
dong_shuai_wen <dongshuaiwenchina@163.com>
2022-08-10 18:15:19 +0800

    2、RecycleView的使用：外界主界面调用RecycleView、加载数据；并注释调用ListView的代码

--- app/src/main/java/com/example/androidlearning/MainActivity.java
+++ app/src/main/java/com/example/androidlearning/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.androidlearning;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -20,8 +22,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // 关联主页面UI
-        setContentView(R.layout.activity_main);
+        // MainActivity主页面逻辑 关联 主页面UI
+//        setContentView(R.layout.listview); // ListView界面
+        setContentView(R.layout.recycleview); // RecycleView界面
 
         // 添加表格数据
         for (int i = 0; i < 100; i++) {
@@ -31,15 +34,25 @@ public class MainActivity extends AppCompatActivity {
         }
 
         // 通过id拿到UI界面上的ListView对象
-        ListView listView = findViewById(R.id.lv);
-        // adapter辅助适配类：用于控件的数据填充（数据和self自己 放入adapter里作为参数）
-        listView.setAdapter(new MyAdapter(listData, this));
-        // listView的item点击事件
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Log.e("点击了", "onItemClick: 第" + i + "行" );
-            }
-        });
+//        ListView listView = findViewById(R.id.lv);
+//        // adapter辅助适配类：用于控件的数据填充（数据和self自己 放入adapter里作为参数）
+//        listView.setAdapter(new ListViewAdapter(listData, this));
+//        // listView的item点击事件
+//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//                Log.e("点击了", "onItemClick: 第" + i + "行" );
+//            }
+//        });
+
+
+        // 通过id拿到UI界面上的RecycleView对象
+        RecyclerView recyclerView = findViewById(R.id.rv);
+        // RecyclerView需要自定义布局（ListView默认就是线性布局）
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        // adapter辅助适配类
+        RecycleViewAdapter recycleAdapter = new RecycleViewAdapter(listData, this);
+        recyclerView.setAdapter(recycleAdapter);
     }
 }
