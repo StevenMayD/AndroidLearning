81dcad408f0716552b87e0e0f623a364585102c2
dong_shuai_wen <dongshuaiwenchina@163.com>
2022-08-11 18:49:33 +0800

    3、动画-逐帧动画：调用逐帧动画，添加主界面加载选择判断

--- app/src/main/java/com/example/androidlearning/MainActivity.java
+++ app/src/main/java/com/example/androidlearning/MainActivity.java
@@ -6,12 +6,15 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,15 +23,27 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
     // 数组data用于给ListView加载数据（数组中是Bean类型元素）
     private List<Bean> listData = new ArrayList<>();
-
+    private boolean flag = true;
+    private String loadSelector = "framebyframe_animation"; // 配置主视图加载的 主界面
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // MainActivity主页面逻辑 关联 主页面UI
-//        setContentView(R.layout.listview); // ListView界面
-        setContentView(R.layout.recycleview); // RecycleView界面
+        if (loadSelector == "listview") {
+            // MainActivity主页面逻辑Java 关联 主页面UI布局layout
+            setContentView(R.layout.listview); // ListView界面
+            loadListView();
+        } else if (loadSelector == "recycleview") {
+            setContentView(R.layout.recycleview); // RecycleView界面
+            loadRecyclerView();
+        } else if (loadSelector == "framebyframe_animation") {
+            setContentView(R.layout.framebyframe_animation); // RecycleView界面
+            loadFrameByFrameAnimationView();
+        }
+    }
 
+//    加载ListView界面
+    private void loadListView() {
         // 添加表格数据
         for (int i = 0; i < 1009; i++) {
             Bean bean = new Bean();
@@ -37,17 +52,26 @@ public class MainActivity extends AppCompatActivity {
         }
 
         // 通过id拿到UI界面上的ListView对象
-//        ListView listView = findViewById(R.id.lv);
-//        // adapter辅助适配类：用于控件的数据填充（数据和self自己 放入adapter里作为参数）
-//        listView.setAdapter(new ListViewAdapter(listData, this));
-//        // listView的item点击事件
-//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-//                Log.e("点击了", "onItemClick: 第" + i + "行" );
-//            }
-//        });
+        ListView listView = findViewById(R.id.lv);
+        // adapter辅助适配类：用于控件的数据填充（数据和self自己 放入adapter里作为参数）
+        listView.setAdapter(new ListViewAdapter(listData, this));
+        // listView的item点击事件
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Log.e("点击了", "onItemClick: 第" + i + "行" );
+            }
+        });
+    }
 
+    //    加载RecyclerView界面
+    private void loadRecyclerView() {
+        // 添加表格数据
+        for (int i = 0; i < 1009; i++) {
+            Bean bean = new Bean();
+            bean.setName("你好呀 " + i);
+            listData.add(bean);
+        }
 
         // 通过id拿到UI界面上的RecycleView对象
         RecyclerView recyclerView = findViewById(R.id.rv);
@@ -73,6 +97,23 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void itemClick(int position) {
                 Log.e("RecycleView点击了", "onRecycleItemClick: 第" + position + "个");
+            }
+        });
+    }
+
+//    加载逐帧动画界面
+    private void loadFrameByFrameAnimationView() {
+        RelativeLayout relativeLayout = findViewById(R.id.frameByFrameAnimation); // 获取到页面的布局
+        AnimationDrawable anim = (AnimationDrawable) relativeLayout.getBackground(); // 获取到布局的背景图 为animation-list 用AnimationDrawable动画类接收
+        relativeLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (flag) {
+                    anim.start(); // 动画启动
+                } else  {
+                    anim.stop(); // 动画停止
+                }
+                flag = !flag;
             }
         });
     }
