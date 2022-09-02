68cfbd2cf85c597ecb3b74a5a0072dbca8deefc7
dong_shuai_wen <dongshuaiwenchina@163.com>
2022-09-02 17:51:34 +0800

    10、集成高德地图 - 使用高德地图：定位（获取当前位置详细参数， 经纬度、国家、省市区、街道）

--- app/src/main/java/com/example/androidlearning/view/activity/MapActivity.java
+++ app/src/main/java/com/example/androidlearning/view/activity/MapActivity.java
@@ -14,9 +14,19 @@ import com.amap.api.maps.AMap; // 地图控制器对象
 import com.amap.api.maps.model.MyLocationStyle; // 定位样式
 import com.amap.api.maps.MapsInitializer;
 
-public class MapActivity extends AppCompatActivity {
-    private MapView mMapView; // 地图控件
-    private AMap aMap = null; // 地图控制器对象
+import com.amap.api.location.AMapLocation; // 定位
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
+
+// 继承  AMapLocationListener（多继承 以逗号分隔）
+public class MapActivity extends AppCompatActivity implements AMapLocationListener {
+    private MapView mMapView; // 地图控件（显示地图画面）
+    private AMap aMap = null; // 地图控制器对象（显示定位蓝点）
+
+    public AMapLocationClient mlocationClient; // 定位客户端对象（定位详细参数信息-经纬度，省市区）
+    public AMapLocationClientOption mLocationOption = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,14 +56,75 @@ public class MapActivity extends AppCompatActivity {
         aMap.getUiSettings().setMyLocationButtonEnabled(true); // 设置默认定位按钮是否显示，非必需设置。
         aMap.setMyLocationEnabled(true);// 设置为true表示启动显示定位蓝点，false表示隐藏定位蓝点并不进行定位，默认是false。
         aMap.moveCamera(CameraUpdateFactory.zoomTo(15)); // 指定地图显示级别为15级
-        aMap.setOnMyLocationChangeListener(new AMap.OnMyLocationChangeListener() { // 定位回调监听器
+        // 定位回调监听器：获得经纬度信息
+        aMap.setOnMyLocationChangeListener(new AMap.OnMyLocationChangeListener() {
             @Override
             public void onMyLocationChange(Location location) {
                 double lat = location.getLatitude();
                 double lng = location.getLongitude();
-                Log.d("duo", "onMyLocationChange: lat=" + lat + "|lng=" + lng);
+                Log.d("MapActivity", "onMyLocationChange: 纬度：" + lat + "| 经度：" + lng);
             }
         });
+
+        try {
+            mlocationClient = new AMapLocationClient(this);
+            //初始化定位参数
+            mLocationOption = new AMapLocationClientOption();
+            //设置定位监听
+            mlocationClient.setLocationListener((AMapLocationListener) this);
+            //设置定位模式为高精度模式，Battery_Saving为低功耗模式，Device_Sensors是仅设备模式
+            mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+            //设置定位间隔,单位毫秒,默认为2000ms
+            mLocationOption.setInterval(2000);
+            //设置定位参数
+            mlocationClient.setLocationOption(mLocationOption);
+            // 此方法为每隔固定时间会发起一次定位请求，为了减少电量消耗或网络流量消耗，
+            // 注意设置合适的定位时间的间隔（最小间隔支持为1000ms），并且在合适时间调用stopLocation()方法来取消定位请求
+            // 在定位结束后，在合适的生命周期调用onDestroy()方法
+            // 在单次定位情况下，定位无论成功与否，都无需调用stopLocation()方法移除请求，定位sdk内部会移除
+
+            //启动定位
+            mlocationClient.startLocation();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // 实现AMapLocationListener的方法 定位信息回调
+    @Override
+    public void onLocationChanged(AMapLocation amapLocation) {
+        if (amapLocation != null) {
+            if (amapLocation.getErrorCode() == 0) {
+                //定位成功回调信息，设置相关消息
+                amapLocation.getLocationType();//获取当前定位结果来源，如网络定位结果，详见定位类型表
+                double latitude = amapLocation.getLatitude();//获取纬度
+                double longitude = amapLocation.getLongitude();//获取经度
+                float accuracy = amapLocation.getAccuracy();//获取精度信息
+                String address = amapLocation.getAddress();//地址，如果option中设置isNeedAddress为false，则没有此结果，网络定位结果中会有地址信息，GPS定位不返回地址信息。
+                String country = amapLocation.getCountry();//国家信息
+                String province = amapLocation.getProvince();//省信息
+                String city = amapLocation.getCity();//城市信息
+                String district = amapLocation.getDistrict();//城区信息
+                String street = amapLocation.getStreet();//街道信息
+                String streetNum = amapLocation.getStreetNum();//街道门牌号信息
+                String cityCode = amapLocation.getCityCode();//城市编码
+                String adCode = amapLocation.getAdCode();//地区编码
+                String aoiName = amapLocation.getAoiName();//获取当前定位点的AOI信息
+                /*
+                * 定位信息为：纬度：34.196786，经度：108.887569，精度信息：61.0，地址：陕西省西安市雁塔区丈八东路1号都靠近绿地都市之门
+                * 国家：中国，省：陕西省，市：西安市，区：雁塔区，街道：丈八东路，街道门牌号：1号都，城市编码：029，地区编码：610113，当前定位点的AOI信息：都市之门C座
+                * */
+                Log.d("onLocationChanged", "定位信息为：" +
+                        "纬度：" + latitude + "，经度：" + longitude + "，精度信息：" + accuracy + "，地址：" + address +
+                        "，国家：" + country + "，省：" + province + "，市：" + city + "，区：" + district + "，街道：" + street + "，街道门牌号：" + streetNum +
+                        "，城市编码：" + cityCode + "，地区编码：" + adCode + "，当前定位点的AOI信息：" + aoiName);
+            } else {
+                //显示错误信息ErrCode是错误码，errInfo是错误信息，详见错误码表。
+                Log.e("AmapError","location Error, ErrCode:"
+                        + amapLocation.getErrorCode() + ", errInfo:"
+                        + amapLocation.getErrorInfo());
+            }
+        }
     }
 
     @Override
