54ebf2884ff3bd662b975209674392be0adc265c
dong_shuai_wen <dongshuaiwenchina@163.com>
2022-12-28 18:19:25 +0800

    16、Intent： 2. intent传递bundle（基础类型数据包裹）

--- app/src/main/java/com/example/androidlearning/view/activity/intentstudy/IntentActivity1.java
+++ app/src/main/java/com/example/androidlearning/view/activity/intentstudy/IntentActivity1.java
@@ -17,13 +17,28 @@ public class IntentActivity1 extends AppCompatActivity {
     }
 
     /**
-     * 跳转
+     * 跳转：intent传递基本类型数据
      * */
     public void startAction(View view) {
         Intent intent = new Intent(this, IntentActivity2.class);
         // 跳转到IntentActivity2时 携带数据（intent可以传递基本数据类型 不能传递对象）
         intent.putExtra("name", "李连杰"); // 传递String字符串
         intent.putExtra("sex", 'M'); // 传递char字符
+        startActivity(intent);
+    }
+
+    /**
+     * 跳转：intent传递bundle包装数据
+     * */
+    public void startAction2(View view) {
+        Intent intent = new Intent(this, IntentActivity2.class);
+
+        // 封装包裹 bundle对象
+        Bundle bundle = new Bundle();
+        bundle.putString("name", "吕布");
+        bundle.putChar("sex", 'M');
+
+        intent.putExtras(bundle);
         startActivity(intent);
     }
 }
--- app/src/main/java/com/example/androidlearning/view/activity/intentstudy/IntentActivity2.java
+++ app/src/main/java/com/example/androidlearning/view/activity/intentstudy/IntentActivity2.java
@@ -15,7 +15,11 @@ public class IntentActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intent2);
 
-        // 接收携带回来的数据
+        //
+        /**
+         * 接收携带回来的数据
+         *  不管数据是零散传递 还是通过包裹bundle传递，接收方一样的处理（如果是bundle包括，会自动拆解bundle）
+         * */
         Intent intent = getIntent();
         String name = intent.getStringExtra("name");
         char sex = intent.getCharExtra("sex", 'A');
